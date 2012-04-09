b823e764ef7b74fb278904c9db3b01f9142be4d8
rocky <rocky@gnu.org>
2012-04-09 14:59:47 -0400

    Small changes

--- load-relative.el
+++ load-relative.el
@@ -6,7 +6,7 @@
 ;; URL: http://github.com/rocky/emacs-load-relative
 ;; Compatibility: GNU Emacs 23.x
 
-;;  Copyright (C) 2009, 2010 Rocky Bernstein <rocky@gnu.org>
+;;  Copyright (C) 2009, 2010, 2012 Rocky Bernstein <rocky@gnu.org>
 
 ;; This program is free software: you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -71,7 +71,7 @@ methods work we will use the file-name value find via
    ;; eval-like things
    ((buffer-file-name))     
 
-   ;; When byte compiling. FIXME: use a more through precondition like
+   ;; When byte compiling. FIXME: use a more thorough precondition like
    ;; byte-compile-file is somehwere in the backtrace or that
    ;; bytecomp-filename comes from that routine? 
    ((boundp 'bytecomp-filename) bytecomp-filename)
@@ -80,8 +80,8 @@ methods work we will use the file-name value find via
    ))
 
 (defun autoload-relative (function-or-list 
-			  file &optional docstring interactive type
-			  symbol)
+                          file &optional docstring interactive type
+                          symbol)
   "autoload an Emacs Lisp file relative to Emacs Lisp code that is in
 the process of being loaded or eval'd.
 
@@ -108,10 +108,10 @@ finding __FILE__ don't work."
 
   (if (listp function-or-list)
       (mapcar (lambda(function)
-		(autoload function-or-list 
-		  (relative-expand-file-name file symbol)
-		  docstring interactive type))
-	      file)
+                (autoload function-or-list 
+                  (relative-expand-file-name file symbol)
+                  docstring interactive type))
+              file)
     (autoload function-or-list (relative-expand-file-name file symbol)
       docstring interactive type))
   )
@@ -127,8 +127,8 @@ if other methods of finding __FILE__ don't work."
 
   (if (listp file-or-list)
       (mapcar (lambda(relative-file)
-		(load (relative-expand-file-name relative-file symbol)))
-	      file-or-list)
+                (load (relative-expand-file-name relative-file symbol)))
+              file-or-list)
     (load (relative-expand-file-name file-or-list symbol)))
   )
 
@@ -139,7 +139,7 @@ the process of being loaded or eval'd.
 WARNING: it is best to to run this function before any
 buffer-setting or buffer changing operations."
   (let ((file (or opt-file (__FILE__) default-directory))
-	(prefix))
+        (prefix))
     (unless file
       (error "Can't expand __FILE__ here and no file name given"))
     (setq prefix (file-name-directory file))
@@ -154,10 +154,10 @@ symbol.
 WARNING: it is best to to run this function before any
 buffer-setting or buffer changing operations."
   (let ((require-string-name 
-	 (concat opt-prefix (file-name-sans-extension 
-			     (file-name-nondirectory relative-file)))))
+         (concat opt-prefix (file-name-sans-extension 
+                             (file-name-nondirectory relative-file)))))
     (require (intern require-string-name) 
-	     (relative-expand-file-name relative-file opt-file))))
+             (relative-expand-file-name relative-file opt-file))))
 
 (defmacro require-relative-list (list &optional opt-prefix)
   "Run `require-relative' on each name in LIST which should be a list of
@@ -165,8 +165,8 @@ strings, each string being the relative name of file you want to run."
   `(progn 
      (eval-when-compile
        (require 'cl
-		(dolist (rel-file ,list)
-		  (require-relative rel-file (__FILE__) ,opt-prefix))))
+                (dolist (rel-file ,list)
+                  (require-relative rel-file (__FILE__) ,opt-prefix))))
      (dolist (rel-file ,list)
        (require-relative rel-file (__FILE__) ,opt-prefix))))
 
@@ -176,7 +176,7 @@ source-code's file basename sans extension. For example if you
 write (provide-me) inside file ~/lisp/foo.el, this is the same as
 writing: (provide 'foo)."
   `(provide (intern (concat ,prefix (file-name-sans-extension
-				     (file-name-nondirectory (__FILE__)))))))
+                                     (file-name-nondirectory (__FILE__)))))))
 
 (provide-me)
 
