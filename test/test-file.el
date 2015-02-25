84f4a5e06c634a8c867222d21596d2e0aa876263
Stefan Monnier <monnier@iro.umontreal.ca>
2015-02-25 12:31:03 -0500

    * load-relative/test/test-file.el: Fix up copyright

--- test/test-file.el
+++ test/test-file.el
@@ -1,4 +1,26 @@
+;;; test-file.el --- Simple test for load-relative
+
+;; Copyright (C) 2015  Free Software Foundation, Inc.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
 (load-file "../load-relative.el")
+
 (require 'ert)
 
 
@@ -34,3 +56,7 @@
     (with-relative-file
         "simple.txt"
       (buffer-string)))))
+
+(provide 'test-file)
+
+;;; test-file.el ends here
